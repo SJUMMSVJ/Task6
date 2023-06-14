eefa9428ca4e4d9adbe4dad40dba6331be987c11
Venkat <115686382+SJUMMSVJ@users.noreply.github.com>
2023-06-14 11:03:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+class price {
+    constructor(km, cost) {
+        this.km=km;
+        this.cost=cost;
+        
+    }
+}
+greet = () => {
+return `${km * cost}`;
+}
+var myprice=new price(12, 22)
+console.log(`travel charge is ${12*22}`);
